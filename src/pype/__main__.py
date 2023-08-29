a16cc072fffe5712ddca86dca83838260868c49c
Naoya Yamashita <conao3@gmail.com>
2023-08-30 00:21:59 +0900

    add argparse

--- src/pype/__main__.py
+++ src/pype/__main__.py
@@ -1,2 +1,13 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', help='execute a command')
+
+    return parser.parse_args()
+
+
 def main():
-    print('hello')
+    args = parse_args()
+    print(args)
