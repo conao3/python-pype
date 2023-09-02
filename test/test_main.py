b8e1debb0016e53f15cebbec3dd331375202cb0a
Naoya Yamashita <conao3@gmail.com>
2023-09-02 10:20:31 +0900

    add -m

--- test/test_main.py
+++ test/test_main.py
@@ -140,3 +140,38 @@ with open("tmp.fifo") as f:
         print("hello")
 '''
     assert_stdout(proc, expected)
+
+
+def test_2811f31f():
+    proc = run_pype(['-c', '-m', 'datetime', 'os.path[join,exists]', '-nle', 'print("hello")'])
+    expected = '''\
+import sys
+import datetime
+from os.path import join,exists
+
+with open("tmp.fifo") as f:
+    for NR, line in enumerate(f, start=1):
+        line = line.strip()
+        L = line
+        _ = L
+        print("hello")
+'''
+    assert_stdout(proc, expected)
+
+
+def test_ee6aaa3b():
+    proc = run_pype(['-c', '-m', 'os', 'datetime=dt', 'os.path[join=jn,exists]', '-nle', 'print("hello")'])
+    expected = '''\
+import sys
+import os
+import datetime as dt
+from os.path import join as jn,exists
+
+with open("tmp.fifo") as f:
+    for NR, line in enumerate(f, start=1):
+        line = line.strip()
+        L = line
+        _ = L
+        print("hello")
+'''
+    assert_stdout(proc, expected)
