98b50d472176d3796322adc48801f6c2e2e0473e
Naoya Yamashita <conao3@gmail.com>
2023-09-03 01:24:58 +0900

    remove unneeded sys import

--- test/test_main.py
+++ test/test_main.py
@@ -13,8 +13,7 @@ def run_pype(args: list[str]) -> subprocess.CompletedProcess[bytes]:
 def test_91d1d7b1():
     proc = run_pype(['-c', '-e', 'print("hello")'])
     expected = '''\
-import sys
-
+# pype
 with open("tmp.fifo") as f:
     line = f.read()
     __ors = ''
@@ -28,8 +27,7 @@ with open("tmp.fifo") as f:
 def test_bb9de651():
     proc = run_pype(['-c', '-e', 'print("hello")', 'print("world")'])
     expected = '''\
-import sys
-
+# pype
 with open("tmp.fifo") as f:
     line = f.read()
     __ors = ''
@@ -44,8 +42,7 @@ with open("tmp.fifo") as f:
 def test_04da38f4():
     proc = run_pype(['-c', '-ne', 'print("hello")'])
     expected = '''\
-import sys
-
+# pype
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = ''
@@ -59,8 +56,7 @@ with open("tmp.fifo") as f:
 def test_c604fcbf():
     proc = run_pype(['-c', '-nle', 'print("hello")'])
     expected = '''\
-import sys
-
+# pype
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -75,9 +71,8 @@ with open("tmp.fifo") as f:
 def test_66260b0f():
     proc = run_pype(['-c', '-a', '-nle', 'print("hello")'])
     expected = '''\
-import sys
+# pype
 import re as __re
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -95,9 +90,8 @@ with open("tmp.fifo") as f:
 def test_ed4ca3c4():
     proc = run_pype(['-c', '-aF,', '-nle', 'print(F[0])'])
     expected = '''\
-import sys
+# pype
 import re as __re
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -115,9 +109,8 @@ with open("tmp.fifo") as f:
 def test_e089f925():
     proc = run_pype(['-c', '-aF-', '-nle', 'print(F[0])'])
     expected = '''\
-import sys
+# pype
 import re as __re
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -135,9 +128,8 @@ with open("tmp.fifo") as f:
 def test_0dc91605():
     proc = run_pype(['-c', r"-aF\s", '-nle', 'print(F[0])'])
     expected = '''\
-import sys
+# pype
 import re as __re
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -155,9 +147,8 @@ with open("tmp.fifo") as f:
 def test_dce9b4ec():
     proc = run_pype(['-c', '-m', 'datetime', '-nle', 'print("hello")'])
     expected = '''\
-import sys
+# pype
 import datetime
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -172,10 +163,9 @@ with open("tmp.fifo") as f:
 def test_468cf587():
     proc = run_pype(['-c', '-m', 'datetime', 're', '-nle', 'print("hello")'])
     expected = '''\
-import sys
+# pype
 import datetime
 import re
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -190,10 +180,9 @@ with open("tmp.fifo") as f:
 def test_2811f31f():
     proc = run_pype(['-c', '-m', 'datetime', 'os.path[join,exists]', '-nle', 'print("hello")'])
     expected = '''\
-import sys
+# pype
 import datetime
 from os.path import join,exists
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -208,11 +197,10 @@ with open("tmp.fifo") as f:
 def test_ee6aaa3b():
     proc = run_pype(['-c', '-m', 'os', 'datetime=dt', 'os.path[join=jn,exists]', '-nle', 'print("hello")'])
     expected = '''\
-import sys
+# pype
 import os
 import datetime as dt
 from os.path import join as jn,exists
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -227,10 +215,9 @@ with open("tmp.fifo") as f:
 def test_b6da375e():
     proc = run_pype(['-c', '-m', 'datetime[*]', '-M', 're', '-nle', 'print("hello")'])
     expected = '''\
-import sys
+# pype
 from datetime import *
 from re import *
-
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -245,8 +232,7 @@ with open("tmp.fifo") as f:
 def test_ffd5b397():
     proc = run_pype(['-c', '-nlp'])
     expected = '''\
-import sys
-
+# pype
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
@@ -261,8 +247,7 @@ with open("tmp.fifo") as f:
 def test_f9105054():
     proc = run_pype(['-c', '-nlp', '-e', 'line = line[:-1]'])
     expected = '''\
-import sys
-
+# pype
 with open("tmp.fifo") as f:
     for NR, line in enumerate(f, start=1):
         __ors = None
